a8626f0b64fd6518aa19b8693df1efcf0d9afa68
Oleksandr <xander_uzzzer@mail.ru>
2017-04-10 15:54:16 +0300

    1004171554

---
+++ Lesson6.HomeTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6.HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LibraryUser:
+            //AddBook
+            //RemoveBook
+            //BooksCount - count of reserverd book
+            //BookInfo - returns book name by index
+            //Indexer - returns book index by name
+
+        }
+    }
+}
